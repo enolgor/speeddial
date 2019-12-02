df244c18816b61b2e55f08d55088447c43c9d52a
enolgor <enolgor@teleco.upv.es>
2019-12-02 13:49:47 +0100

    Version: 2.2

--- .travis/push.sh
+++ .travis/push.sh
@@ -11,8 +11,8 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/enolgor/speeddial.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin master
+  git remote add origin-pages https://${GH_TOKEN}@github.com/enolgor/speeddial.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages master
 }
 
 setup_git
